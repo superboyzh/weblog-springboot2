41a67463eb0c8bd3d7f966044433cae1d784eaac
zenghao <zengh@shjysoft.com>
2025-04-02 17:20:02 +0800

    整合Logback日志

--- weblog-web/src/main/java/com/quanxiaoha/weblog/web/WeblogWebApplication.java
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/WeblogWebApplication.java
@@ -1,4 +1,4 @@
-package com.quanxiaoha.weblogweb;
+package com.quanxiaoha.weblog.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
