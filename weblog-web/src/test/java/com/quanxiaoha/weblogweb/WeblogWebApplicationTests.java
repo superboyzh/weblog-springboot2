ade7a4884a6b90869551b58089da840f7584e2f5
zenghao <zengh@shjysoft.com>
2025-04-02 16:03:39 +0800

    项目初始化

---
+++ weblog-web/src/test/java/com/quanxiaoha/weblogweb/WeblogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanxiaoha.weblogweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
