5063b7eaa0e1349ed6fb773deb4b88ff7e4180f7
zenghao <zengh@shjysoft.com>
2025-04-03 09:39:24 +0800

    自定义响应工具类

--- weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.quanxiaoha.weblog.web.controller;
 
+import com.quanxiaoha.weblog.common.utils.Response;
 import com.quanxiaoha.weblog.web.model.User;
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +20,7 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
         // 是否存在校验错误
         if (bindingResult.hasErrors()) {
             // 获取校验不通过字段的提示信息
@@ -28,10 +29,10 @@ public class TestController {
                     .map(FieldError::getDefaultMessage)
                     .collect(Collectors.joining(", "));
 
-            return ResponseEntity.badRequest().body(errorMsg);
+            return Response.fail(errorMsg);
         }
         // 返参
-        return ResponseEntity.ok("参数没有任何问题");
+        return Response.success();
     }
     @GetMapping("/test2")
     @ApiOperationLog(description = "测试接口2")
