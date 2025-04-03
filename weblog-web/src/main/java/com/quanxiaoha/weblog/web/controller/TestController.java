63fd1ef28af56785e7578414eeae55d376489dea
zenghao <zengh@shjysoft.com>
2025-04-03 14:02:23 +0800

    全局异常处理器加参数校验

--- weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
@@ -22,15 +22,15 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
-
+    public Response test(@RequestBody @Validated User user) {
+        return Response.success();
         //1. 处理参数校验异常
         // 手动抛异常，入参是前面定义好的异常码枚举，返参统一交给全局异常处理器搞定
         // throw new BizException(ResponseCodeEnum.PRODUCT_NOT_FOUND);
         // 2
         //主动定义一个运行时异常，分母不能为零
-        int i = 1 / 0;
-        return Response.success();
+//        int i = 1 / 0;
+//        return Response.success();
         // 3
         // 是否存在校验错误
 //        if (bindingResult.hasErrors()) {
